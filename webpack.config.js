052a339d20ff88c8af327a104835681fa33f9e58
Greg Bowne <gbowne144@gmail.com>
2021-05-15 15:18:41 -0700

    somewhat changed & partially fixed

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require("webpack");
+
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          loader: 'style!css!sass?outputStyle=expanded',
+        },
+      ], // was a semicolon
+
+      plugins: [
+        {
+          new BundleAnalyzerPlugin(),
+        },
+      ], //was a semicolon
+
+      devServer: [
+      {
+          contentBase: path.join(__dirname, 'public'),
+          port: 9000,
+      },
+      ], //was a semicolon
+    },
+};
