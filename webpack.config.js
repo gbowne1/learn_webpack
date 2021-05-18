0b367ce6a40aef8019953764901dc7e379440100
Gregory Bowne <47549872+gbowne1@users.noreply.github.com>
2021-05-18 13:49:51 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       devServer: [
       {
           contentBase: path.join(__dirname, 'public'),
-          port: 9000,
+          port: 8080,
       },
       ], //was a semicolon
     },
